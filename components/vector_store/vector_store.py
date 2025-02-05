288bb93bab802910cd04fa847c492d5f321838a5
Karna Vetrivel <karna.vetrivel@freshworks.com>
2025-02-05 22:03:33 +0530

    Update vector_store.py

--- components/vector_store/vector_store.py
+++ components/vector_store/vector_store.py
@@ -57,7 +57,10 @@ class VectorStore:
                         ssl_context=ssl_context,
                     )
                 else:
-                    self.store = OpenSearch([es_host], http_auth=())
+                    self.store = OpenSearch(
+                                [es_host],
+                                verify_certs=True  # Ensures proper SSL verification
+                            )
                 self.store.cluster.health()
                 logger.info("Successfully initialized OpenSearch client")
                 self.store_vectors_func = self.store_vectors_os
